325becbc8aa80f0845dfa9644a11b2b4d22516dd
Tanisha <107809690+Tanishaaaaaaa@users.noreply.github.com>
2024-06-06 00:43:57 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,24 +4,6 @@ Open In Colab
 import pandas as pd
 df=pd.read_csv('/content/salaries.csv')
 df
-     
-company	job	degree	salary_more_then_100k
-0	google	sales executive	bachelors	0
-1	google	sales executive	masters	0
-2	google	business manager	bachelors	1
-3	google	business manager	masters	1
-4	google	computer programmer	bachelors	0
-5	google	computer programmer	masters	1
-6	abc pharma	sales executive	masters	0
-7	abc pharma	computer programmer	bachelors	0
-8	abc pharma	business manager	bachelors	0
-9	abc pharma	business manager	masters	1
-10	facebook	sales executive	bachelors	1
-11	facebook	sales executive	masters	1
-12	facebook	business manager	bachelors	1
-13	facebook	business manager	masters	1
-14	facebook	computer programmer	bachelors	1
-15	facebook	computer programmer	masters	1
 
 input=df.drop('salary_more_then_100k',axis='columns')
 target=df['salary_more_then_100k']
@@ -30,43 +12,8 @@ target=df['salary_more_then_100k']
 
 input
      
-company	job	degree
-0	google	sales executive	bachelors
-1	google	sales executive	masters
-2	google	business manager	bachelors
-3	google	business manager	masters
-4	google	computer programmer	bachelors
-5	google	computer programmer	masters
-6	abc pharma	sales executive	masters
-7	abc pharma	computer programmer	bachelors
-8	abc pharma	business manager	bachelors
-9	abc pharma	business manager	masters
-10	facebook	sales executive	bachelors
-11	facebook	sales executive	masters
-12	facebook	business manager	bachelors
-13	facebook	business manager	masters
-14	facebook	computer programmer	bachelors
-15	facebook	computer programmer	masters
 
 target
-     
-0     0
-1     0
-2     1
-3     1
-4     0
-5     1
-6     0
-7     0
-8     0
-9     1
-10    1
-11    1
-12    1
-13    1
-14    1
-15    1
-Name: salary_more_then_100k, dtype: int64
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -84,24 +31,6 @@ input['degreeEnc']=le_company.fit_transform(input['degree'])
      
 
 input
-     
-company	job	degree	companyEnc	jobEnc	degreeEnc
-0	google	sales executive	bachelors	2	2	0
-1	google	sales executive	masters	2	2	1
-2	google	business manager	bachelors	2	0	0
-3	google	business manager	masters	2	0	1
-4	google	computer programmer	bachelors	2	1	0
-5	google	computer programmer	masters	2	1	1
-6	abc pharma	sales executive	masters	0	2	1
-7	abc pharma	computer programmer	bachelors	0	1	0
-8	abc pharma	business manager	bachelors	0	0	0
-9	abc pharma	business manager	masters	0	0	1
-10	facebook	sales executive	bachelors	1	2	0
-11	facebook	sales executive	masters	1	2	1
-12	facebook	business manager	bachelors	1	0	0
-13	facebook	business manager	masters	1	0	1
-14	facebook	computer programmer	bachelors	1	1	0
-15	facebook	computer programmer	masters	1	1	1
 
 inputs=input.drop(['company','job','degree'],axis='columns')
      
@@ -114,9 +43,7 @@ model=tree.DecisionTreeClassifier(criterion='gini')
 
 model.fit(inputs,target)
      
-DecisionTreeClassifier()
-In a Jupyter environment, please rerun this cell to show the HTML representation or trust the notebook.
-On GitHub, the HTML representation is unable to render, please try loading this page with nbviewer.org.
+
 
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
